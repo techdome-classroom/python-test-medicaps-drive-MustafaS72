3d216290de538cc81459df0eebf0fd1fde44704f
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 13:56:40 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,4 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        # Initialize the total value and previous value
+        total = 0
+        prev_value = 0
+        
+        # Iterate through the string from right to left
+        for char in reversed(s):
+            value = roman_values[char]
+            
+            # If current value is less than previous value, it's a subtraction case
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            
+            # Update the previous value for the next iteration
+            prev_value = value
+        
+        return total
