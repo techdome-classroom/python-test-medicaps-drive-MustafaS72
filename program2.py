2be44701ea577bc7900ff416b7ae47cbea04aedb
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 13:58:11 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -9,21 +9,17 @@ class Solution(object):
             'C': 100, 'D': 500, 'M': 1000
         }
         
-        # Initialize the total value and previous value
         total = 0
         prev_value = 0
         
-        # Iterate through the string from right to left
         for char in reversed(s):
             value = roman_values[char]
             
-            # If current value is less than previous value, it's a subtraction case
             if value < prev_value:
                 total -= value
             else:
                 total += value
             
-            # Update the previous value for the next iteration
             prev_value = value
         
         return total
