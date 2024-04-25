df171b2d03b6f12a91807249ec84c9069786c282
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 13:54:22 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-    
-
-
-
-  
-
+        stack = []
+        mapping = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in mapping:
+                # If the current character is a closing bracket
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # If the current character is an opening bracket
+                stack.append(char)
+        
+        # If the stack is empty at the end, all parentheses are correctly matched
+        return not stack
